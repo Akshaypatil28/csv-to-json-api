9be9048c80d0ee3ad0635666787f104b7284b458
Akshay Patil <akshay.patil@pubmatic.com>
2024-04-06 18:53:55 +0530

    add changes in env files

--- src/envconfig/index.ts
+++ src/envconfig/index.ts
@@ -1,14 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import path from "path";
 
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const envFound = dotenv.config();
+const envFound = dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-if (envFound.error) {
+if (!envFound) {
   throw new Error("Couldn't find .env file");
 }
 
-export default {
-  port: parseInt(process.env.PORT) || 3000,
-}
+module.exports = {
+  port: parseInt(process.env.PORT!) || 3000,
+  CSV_FILE_PATH: process.env.CSV_FILE_PATH!,
+  PG_USER: process.env.PG_USER!,
+  PG_HOST: process.env.PG_HOST!,
+  PG_DATABASE: process.env.PG_DATABASE!,
+  PG_PASSWORD: process.env.PG_PASSWORD!,
+  PG_PORT: parseInt(process.env.PG_PORT!) || 5432,
+};
