d69601b7979b219924281c9e674e1c57ad4b8992
Akshay Patil <akshay.patil@pubmatic.com>
2024-04-07 21:08:35 +0530

    code refactoring

--- src/helper/person-details.helper.ts
+++ src/helper/person-details.helper.ts
@@ -1,9 +1,9 @@
-export const getPersonDetails = function(obj: any) {
-  const { firstName, lastName } = obj.name;
+export const getPersonDetails = function(personDetails: any) {
+  const { firstName, lastName } = personDetails.name;
   const name = `${firstName} ${lastName}`;
-  const age = parseInt(obj.age, 10);
-  const address = obj.address ? JSON.stringify(obj.address) : null;
-  const additionalInfo = JSON.stringify(obj.additional_info || {});
+  const age = parseInt(personDetails.age, 10);
+  const address = personDetails.address ? JSON.stringify(personDetails.address) : null;
+  const additionalInfo = JSON.stringify(personDetails.additional_info || {});
   const values = [name, age, address, additionalInfo];
   return values;
 }
